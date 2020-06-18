0ebbc1d7399c4dcec972721712b15e53b777fa30
quppeq <quppeq@gmail.com>
2020-06-18 21:35:31 +0300

    add history

--- radio/view/radio/__init__.py
+++ radio/view/radio/__init__.py
@@ -1,10 +1,13 @@
 from flask import current_app, jsonify, request, abort, g, render_template
 from flask.views import MethodView
 from db import db
+from sqlalchemy import func
 from radio.models.user import User
+from radio.models.radio import Queue, Track, PartOfDayEnum
 from radio.lib.radio import get_now
 from radio.lib.decorators import authorize
 import logging
+from datetime import datetime
 
 log = logging.getLogger(__name__)
 
@@ -21,4 +24,20 @@ class RadioHistoryView(MethodView):
 
     @authorize
     def get(self):
-        return ""
+        today = datetime.now().date()
+        morning = db.session.query(Queue).filter(
+            Queue.part == PartOfDayEnum.morning.value,
+            Queue.day == today,
+        ).all()
+
+        afternoon = db.session.query(Queue).filter(
+            Queue.part == PartOfDayEnum.afternoon.value,
+            Queue.day == today,
+        ).all()
+
+        evening = db.session.query(Queue).filter(
+            Queue.part == PartOfDayEnum.evening.value,
+            Queue.day == today,
+        ).all()
+
+        return render_template('radio/history.jinja', morning=morning, afternoon=afternoon, evening=evening)
